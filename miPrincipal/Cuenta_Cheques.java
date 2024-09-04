81346d60f50e1344dc3772df563f6b2e46411597
Sebastian Favela Gaxiola <159967496+SebastianFavela@users.noreply.github.com>
2024-09-04 14:56:30 +0000

    correccion

--- miPrincipal/Cuenta_Cheques.java
+++ miPrincipal/Cuenta_Cheques.java
@@ -2,6 +2,52 @@ package miPrincipal;
 
 public class Cuenta_Cheques {
    //coloca el codigo faltante
-    
-    
+   private String NombreCuenta;
+   private String NombreCliente; 
+   private String Sucursal;
+   private float Saldo;
+   
+   public Cuenta_Cheques(String nombreCuenta, String nombreCliente, String sucursal, float saldo) {
+      NombreCuenta = nombreCuenta;
+      NombreCliente = nombreCliente;
+      Sucursal = sucursal;
+      Saldo = saldo;
+   }
+
+
+   
+   public String getNombreCuenta() {
+      return NombreCuenta;
+   }
+  public Cuenta_Cheques(String nombreCuenta, String nombreCliente, String sucursal) {
+   NombreCuenta = nombreCuenta;
+   NombreCliente = nombreCliente;
+   Sucursal = sucursal;
 }
+
+
+ public void setNombreCuenta(String nombreCuenta) {
+      NombreCuenta = nombreCuenta;
+   }
+   public String getNombreCliente() {
+      return NombreCliente;
+   }
+   public void setNombreCliente(String nombreCliente) {
+      NombreCliente = nombreCliente;
+   }
+   public String getSucursal() {
+      return Sucursal;
+   }
+   public void setSucursal(String sucursal) {
+      Sucursal = sucursal;
+   }
+   public float getSaldo() {
+      return Saldo;
+   }
+   public void setSaldo(float saldo) {
+      Saldo = saldo;
+   }
+   
+    }
+    
+
